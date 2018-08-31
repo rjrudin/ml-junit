5b224d26238db86eb1b0270cc4f2889d15d79b96
Rob Rudin <rob.rudin@marklogic.com>
2018-08-31 16:46:44 -0400

    Initial progress

--- src/main/java/com/marklogic/junit/TestHelper.java
+++ src/main/java/com/marklogic/junit/TestHelper.java
@@ -8,15 +8,17 @@ import com.marklogic.xcc.template.XccTemplate;
  * <ol>
  * <li>A DatabaseClientProvider facilities accessing a MarkLogic REST API server via the MarkLogic Java API.</li>
  * <li>An XccTemplate typically connects to an XDBC server that points to the same database as the REST API server. The
- * XccTemplate is useful for performing operations not yet exposed via the REST API server.</li>
+ * XccTemplate is useful for performing operations not yet exposed via the REST API server. Note though that with
+ * MarkLogic 8 and greater, this is typically not needed because a DatabaseClient can be used to accomplish the
+ * same goals via the /v1/eval endpoint.</li>
  * <li>The NamespaceProvider provides an array of JDOM namespaces that can be used in XPath namespaces via JDOM.</li>
  * </ol>
  */
 public interface TestHelper {
 
-    public void setDatabaseClientProvider(DatabaseClientProvider provider);
+	void setDatabaseClientProvider(DatabaseClientProvider provider);
 
-    public void setXccTemplate(XccTemplate xccTemplate);
+	void setXccTemplate(XccTemplate xccTemplate);
 
-    public void setNamespaceProvider(NamespaceProvider namespaceProvider);
+	void setNamespaceProvider(NamespaceProvider namespaceProvider);
 }
--- src/main/java/com/marklogic/junit/spring/AbstractSpringTest.java
+++ src/main/java/com/marklogic/junit/spring/AbstractSpringTest.java
@@ -1,7 +1,6 @@
 package com.marklogic.junit.spring;
 
-import java.util.Map;
-
+import com.marklogic.junit.BaseTestHelper;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
@@ -10,49 +9,38 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.marklogic.client.ext.helper.DatabaseClientProvider;
-import com.marklogic.junit.BaseTestHelper;
-import com.marklogic.xcc.template.XccTemplate;
-
 /**
  * Base class for tests that utilize Spring's support for JUnit. Extends BaseTestHelper so that all of the convenience
  * methods available within that are available to subclasses.
+ * <p>
+ * Aside from using the Spring plumbing, the main purpose of this class is to clear the test database in some fashion.
+ * It defaults to deleting every document in the database, but the exact query that's executed can be specified by
+ * overriding getClearDatabaseXquery.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@TestExecutionListeners({ LoggingTestExecutionListener.class, DependencyInjectionTestExecutionListener.class })
+@TestExecutionListeners({LoggingTestExecutionListener.class, DependencyInjectionTestExecutionListener.class})
 public abstract class AbstractSpringTest extends BaseTestHelper implements ApplicationContextAware {
 
-    private ApplicationContext applicationContext;
+	private ApplicationContext applicationContext;
 
-    @Before
-    public void deleteDocumentsBeforeTestRuns() {
-        XccTemplate xccTemplate = getXccTemplate();
-        if (xccTemplate != null) {
-            xccTemplate.executeAdhocQuery(getClearDatabaseXquery());
-        } else {
-            logger.warn("Not deleting documents before test runs, could not find single instance of "
-                    + XccTemplate.class.getName() + " in Spring");
-        }
-    }
+	@Before
+	public void deleteDocumentsBeforeTestRuns() {
+		getClient().newServerEval().xquery(getClearDatabaseXquery());
+	}
 
-    /**
-     * Protected so a subclass can modify this to, e.g., not delete every document.
-     */
-    protected String getClearDatabaseXquery() {
-        return "cts:uris((), (), cts:and-query(())) ! xdmp:document-delete(.)";
-    }
+	/**
+	 * Protected so a subclass can modify this to, e.g., not delete every document.
+	 */
+	protected String getClearDatabaseXquery() {
+		return "cts:uris((), (), cts:and-query(())) ! xdmp:document-delete(.)";
+	}
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-        setDatabaseClientProvider(applicationContext.getBean(DatabaseClientProvider.class));
-        Map<String, XccTemplate> map = applicationContext.getBeansOfType(XccTemplate.class);
-        if (map.size() == 1) {
-            setXccTemplate(map.values().iterator().next());
-        }
-    }
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
 
-    protected ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
+	protected ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 }
